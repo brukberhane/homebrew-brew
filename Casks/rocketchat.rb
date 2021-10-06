95bc0984c201e05436f3cb0f878f127a1ebf4de7
Bruk Berhane Asfaw <brukbrhane@gmail.com>
2021-10-06 14:49:22 +0300

    added rocketchat

---
+++ Casks/rocketchat.rb
@@ -0,0 +1,12 @@
+cask "rocketchat" do
+	version "3.5.7"
+	sha256 "f0ee490423f3dd44ee6cd83bdfcc34da5082dfb07e6c374638ed84207f449bef"
+
+	url "https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/#{version}/rocketchat-#{version}.dmg",
+		verified: "github.com/RocketChat/Rocket.Chat.Electron"
+	name "Rocket.Chat"
+	desc "Desktop application for Rocket.Chat"
+	homepage "https://rocket.chat"
+
+	app "Rocket.Chat.app"
+end
